3f75e73d1ca68b0ff69e0c8488dbca834f3d358c
Daniel Joshua P. Saberon <152839529+DanielJoshuaSaberon@users.noreply.github.com>
2024-10-05 01:18:06 +0800

    Undo and fix some of the previous changes

--- src/shared/components/ui/button.tsx
+++ src/shared/components/ui/button.tsx
@@ -11,11 +11,10 @@ const buttonVariants = cva(
 			variant: {
 				default: "bg-primary text-primary-foreground hover:bg-primary/90",
 				destructive: "bg-destructive text-destructive-foreground hover:bg-destructive/90",
-				outline: "border bg-background hover:bg-accent",
+				outline: "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
 				secondary: "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-				ghost: "hover:bg-secondary hover:text-accent-foreground group",
+				ghost: "hover:bg-accent hover:text-accent-foreground",
 				link: "text-primary underline-offset-4 hover:underline",
-					
 			},
 			size: {
 				default: "h-10 px-4 py-2",
@@ -23,7 +22,7 @@ const buttonVariants = cva(
 				lg: "h-11 rounded-md px-8",
 				icon: "h-10 w-10",
 				full: "w-full h-28",
-				search:"h-1 rounded-md p-4", 
+				search: "h-1 rounded-md p-4",
 			},
 		},
 		defaultVariants: {
--- src/shared/components/ui/separator.tsx
+++ src/shared/components/ui/separator.tsx
@@ -1,31 +1,26 @@
-'use client'
+"use client";
 
-import * as React from 'react'
-import * as SeparatorPrimitive from '@radix-ui/react-separator'
+import * as React from "react";
+import * as SeparatorPrimitive from "@radix-ui/react-separator";
 
-import { ny } from '@/shared/lib/utils'
+import { ny } from "@/shared/lib/utils";
 
 const Separator = React.forwardRef<
-   React.ElementRef<typeof SeparatorPrimitive.Root>,
-   React.ComponentPropsWithoutRef<typeof SeparatorPrimitive.Root>
->(
-   (
-      { className, orientation = 'horizontal', decorative = true, ...props },
-      ref,
-   ) => (
-      <SeparatorPrimitive.Root
-         ref={ref}
-         decorative={decorative}
-         orientation={orientation}
-         className={ny(
-            'bg-primary-foreground shrink-0',
-            orientation === 'horizontal' ? 'h-px w-full' : 'h-full w-px',
-            className,
-         )}
-         {...props}
-      />
-   ),
-)
-Separator.displayName = SeparatorPrimitive.Root.displayName
+	React.ElementRef<typeof SeparatorPrimitive.Root>,
+	React.ComponentPropsWithoutRef<typeof SeparatorPrimitive.Root>
+>(({ className, orientation = "horizontal", decorative = true, ...props }, ref) => (
+	<SeparatorPrimitive.Root
+		ref={ref}
+		decorative={decorative}
+		orientation={orientation}
+		className={ny(
+			"bg-muted-foreground shrink-0",
+			orientation === "horizontal" ? "h-px w-full" : "h-full w-px",
+			className,
+		)}
+		{...props}
+	/>
+));
+Separator.displayName = SeparatorPrimitive.Root.displayName;
 
-export { Separator }
+export { Separator };
