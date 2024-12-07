79f2bdd979a03968e5984a8c9aa81e3e491b15c4
french <frenchoymontajess@gmail.com>
2024-12-07 15:11:50 +0800

    feat/TV-51-implement-getPermissionsUseCase

--- src/modules/user/src/shared/permissions.ts
+++ src/modules/user/src/shared/permissions.ts
@@ -22,4 +22,71 @@ export const Permissions = {
 	// ! UPDATE THIS WHENEVER YOU ADD NEW PERMISSION
 	ALL: (1 << 14) - 1,
 } as const;
+
+export const PermissionsDetail = {
+	SEARCH_RESOURCES: {
+		name:"Search Resources",
+		desciption: "Allows searching resources in the system.",
+		value: Permissions.SEARCH_RESOURCES,
+	},
+	VIEW_RESOURCES: {
+		name:"View Resources",
+		description:"Allows viewing available resources in the system.",
+		value: Permissions.VIEW_RESOURCES,
+	},
+	UPLOAD_THESIS: {
+		name:"Upload Thesis",
+		description:"Allows uploading theses for review.",
+		value: Permissions.UPLOAD_THESIS,
+	},
+	VIEW_SUBMITTED_THESIS: {
+		name:"View Submitted Thesis",
+		description:"Allows viewing submitted theses for review.",
+		value: Permissions.VIEW_SUBMITTED_THESIS,
+	},
+	MODIFY_THESIS: {
+		name:"Modify Thesis",
+		description:"Allows modifying uploaded theses.",
+		value: Permissions.MODIFY_THESIS,
+	},
+	MANAGE_SUBMITTED_THESES: {
+		name:"Manage Submitted Theses",
+		description:"Allows approving or denying submitted/uploaded theses.",
+		value: Permissions.MANAGE_SUBMITTED_THESES,
+	},
+	DELETE_THESIS: {
+		name:"Delete Thesis",
+		description:"Allows deleting theses.",
+		value: Permissions.DELETE_THESIS,
+	},
+	VIEW_USER_AUDIT_LOG:  {
+		name:"View User Audit Log",
+		description:"Allows viewing account edit history.",
+		value: Permissions.VIEW_USER_AUDIT_LOG,
+	},
+	VIEW_THESIS_AUDIT_LOG: {
+		name:"View Thesis Audit Log",
+		description:"Allows viewing the thesis audit log.",
+		value: Permissions.VIEW_THESIS_AUDIT_LOG,
+	},
+	UPDATE_USER: {
+		name:"Update User",
+		description:"Allows updating user details.",
+		value: Permissions.UPDATE_USER,
+	},
+	DELETE_USER: {
+		name:"Delete User",
+		description:"Allows deleting users.",
+		value: Permissions.DELETE_USER,
+	},
+	MANAGE_PERMISSION: {
+		name:"Manage Permission",
+		description:"Allows granting or revoking permissions and roles for users.",
+		value: Permissions.MANAGE_PERMISSION,
+	},
+
+	// ! UPDATE THIS WHENEVER YOU ADD NEW PERMISSION
+	ALL: Permissions.ALL,
+} as const;
+
 export type PermissionKeys = keyof typeof Permissions;
